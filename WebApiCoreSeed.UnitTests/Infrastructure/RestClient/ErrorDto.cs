610ce16c796b5e633476f031114ff85d0726b8b1
Martin De Sa Granja <mdesagranja@makingsense.com>
2017-05-09 15:34:44 -0300

    [MLP-169] Solution, data and webapi projects created, added entities and context

---
+++ WebApiCoreSeed.UnitTests/Infrastructure/RestClient/ErrorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiCoreSeed.UnitTests.Infrastructure.RestClient
+{
+    public class ErrorDto
+    {
+        public string ErrorDescription { get; set; }
+
+        public int ErrorCode { get; set; }
+    }
+}
---
+++ WebApiCoreSeed.UnitTests/Infrastructure/RestClient/TestResourceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiCoreSeed.UnitTests.Infrastructure.RestClient
+{
+    public class TestResourceDto
+    {
+        public string Id { get; set; }
+
+        public int Code { get; set; }
+    }
+}
