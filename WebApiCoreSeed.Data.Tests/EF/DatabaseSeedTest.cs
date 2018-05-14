03283cd8589c5a551a4ea476c494138d7c32aefb
Gastón Cerioni <dongatasoft@gmail.com>
2018-05-14 15:29:38 -0300

    Adds WebApiCoreSeed.Data.Tests

---
+++ WebApiCoreSeed.Data.Tests/EF/DatabaseSeedTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebApiCoreSeed.Data.EF;
+using Xunit;
+
+namespace WebApiCoreSeed.Data.Tests.EF
+{
+    public class DatabaseSeedTest
+    {
+        [Fact]
+        public async Task Initialize_ShouldAnCreateUser()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<WebApiCoreSeedContext>();
+            optionsBuilder.UseInMemoryDatabase("GetByIdAsync_ShouldReturnUser");
+            var dbContext = new WebApiCoreSeedContext(optionsBuilder.Options);
+            DatabaseSeed.Initialize(dbContext);
+
+            Assert.Equal(1, await dbContext.Users.CountAsync());
+        }
+    }
+}
