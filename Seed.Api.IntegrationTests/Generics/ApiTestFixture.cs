6423deba944f84e7062d221c410cf0325b5a2e52
Gastón Cerioni <dongatasoft@gmail.com>
2018-05-17 19:19:46 -0300

    Issue22dot1 integration tests (#29)
    * decouple unit test project
    
    * removes properties from a test project
    
    * adds integration tests
    
    * removes an unused package

---
+++ Seed.Api.IntegrationTests/Generics/ApiTestFixture.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Seed.Api.IntegrationTests.Fake;
+using Seed.Data.EF;
+using System;
+using System.Net.Http;
+
+namespace Seed.Api.IntegrationTests.Generics
+{
+    public class ApiTestFixture : IDisposable
+    {
+        private readonly TestServer _server;
+
+        public ApiTestFixture()
+        {
+            FileFaker.Fake();
+
+            var builder = new WebHostBuilder()
+                .UseEnvironment("development")
+                .UseStartup<Startup>();
+
+            _server = new TestServer(builder);
+            Client = _server.CreateClient();
+            DbContextOptions = _server.Host.Services
+                .GetService(typeof(DbContextOptions<WebApiCoreSeedContext>)) as DbContextOptions<WebApiCoreSeedContext>;
+        }  
+
+        public HttpClient Client { get; }
+
+        public DbContextOptions<WebApiCoreSeedContext> DbContextOptions { get; }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _server.Dispose();
+        }
+    }
+}
