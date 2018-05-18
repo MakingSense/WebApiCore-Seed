bd0a9b3ae5d1b3837480c2a7390078d9c95fa5a2
Luis Miguel Vittori <luismiguelvittori@gmail.com>
2018-05-18 11:53:21 -0300

    Changes required on comments in PR Domain layer tests #36

--- Seed.Domain.Tests/Services/UserServiceTests.cs
+++ Seed.Domain.Tests/Services/UserServiceTests.cs
@@ -13,54 +13,62 @@ namespace Seed.Domain.Tests
 {
     public class UserServiceTests
     {
+        #region Members
+
+        private readonly Mock<WebApiCoreSeedContext> _context;
+        private readonly UserService _userService;
+
+        #endregion
+
+        #region Constructors
+
+        public UserServiceTests()
+        {
+            _context = new Mock<WebApiCoreSeedContext>();
+            _userService = new UserService(_context.Object);
+        }
+
+        #endregion
+
         #region GetById Tests
 
         [Fact]
         public async Task GetByIdAsync_ShouldReturnUser()
         {
-            // Arrange
-            var context = new Mock<WebApiCoreSeedContext>();
-            var users = new Mock<DbSet<User>>();
-            var createdUser = GetADefaultUser();
-            context.Setup(x => x.Users).Returns(users.Object);
-            users.Setup(x => x.FindAsync(It.Is<Guid>(y => y == createdUser.Id)))
-                 .ReturnsAsync(createdUser)
+            // Arrang
+            var user = GetADefaultUser();
+            _context.Setup(x => x.Users.FindAsync(It.Is<Guid>(y => y == user.Id)))
+                 .ReturnsAsync(user)
                  .Verifiable();
-            var userService = new UserService(context.Object);
+
 
             // Act
-            var retrievedUser = await userService.GetByIdAsync(createdUser.Id);
+            var retrievedUser = await _userService.GetByIdAsync(user.Id);
 
             // Assert
             Assert.NotNull(retrievedUser);
-            Assert.Equal(createdUser.UserName, retrievedUser.UserName);
-            Assert.Equal(createdUser.Email, retrievedUser.Email);
-            Assert.Equal(createdUser.FirstName, retrievedUser.FirstName);
-            Assert.Equal(createdUser.LastName, retrievedUser.LastName);
-            context.VerifyAll();
+            Assert.Equal(user.UserName, retrievedUser.UserName);
+            Assert.Equal(user.Email, retrievedUser.Email);
+            Assert.Equal(user.FirstName, retrievedUser.FirstName);
+            Assert.Equal(user.LastName, retrievedUser.LastName);
+            _context.Verify(c => c.Users.FindAsync(user.Id), Times.Once);
         }
 
         [Fact]
         public async Task GetByIdAsync_ShouldReturnNull()
         {
-            // Arrange
-            var context = new Mock<WebApiCoreSeedContext>();
-            var users = new Mock<DbSet<User>>();
+            // Arrange 
             var id = Guid.NewGuid();
-            User user = null;
-            context.Setup(x => x.Users)
-                   .Returns(users.Object);
-            users.Setup(x => x.FindAsync(It.Is<Guid>(y => y == id)))
-                 .ReturnsAsync(user)
+            _context.Setup(x => x.Users.FindAsync(It.Is<Guid>(y => y == id)))
+                 .ReturnsAsync((User)null)
                  .Verifiable();
-            var userService = new UserService(context.Object);
 
             //Act
-            var retrievedUser = await userService.GetByIdAsync(id);
+            var retrievedUser = await _userService.GetByIdAsync(id);
 
             // Assert
             Assert.Null(retrievedUser);
-            context.VerifyAll();
+            _context.Verify(c => c.Users.FindAsync(id), Times.Once);
         }
 
         #endregion
@@ -70,52 +78,39 @@ namespace Seed.Domain.Tests
         [Fact]
         public async void Delete_ShouldDeleteUser()
         {
-            // Arrange
-            var context = new Mock<WebApiCoreSeedContext>();
-            var users = new Mock<DbSet<User>>();
+            // Arrang
             var createdUser = GetADefaultUser();
-            context.Setup(x => x.Users)
-                   .Returns(users.Object);
-            users.Setup(x => x.FindAsync(It.Is<Guid>(y => y == createdUser.Id)))
+            _context.Setup(x => x.Users.FindAsync(It.Is<Guid>(y => y == createdUser.Id)))
                  .ReturnsAsync(createdUser)
                  .Verifiable();
-            users.Setup(x => x.Remove(It.Is<User>(y => y.Id == createdUser.Id)))
+            _context.Setup(x => x.Users.Remove(It.Is<User>(y => y.Id == createdUser.Id)))
                  .Verifiable();
-            context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
+            _context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
                    .ReturnsAsync(1);
-            var userService = new UserService(context.Object);
 
             // Act
-            var affectedRows = await userService.DeleteByIdAsync(createdUser.Id);
+            var affectedRows = await _userService.DeleteByIdAsync(createdUser.Id);
 
             // Assert
             Assert.True(affectedRows > 0);
-            users.Verify(x => x.Remove(It.Is<User>(y => y.Id == createdUser.Id)), Times.Once);
-            context.VerifyAll();
+            _context.Verify(x => x.Users.Remove(It.Is<User>(y => y.Id == createdUser.Id)), Times.Once);
         }
 
         [Fact]
         public async void Delete_UserNotFound()
         {
-            // Arrange
-            var context = new Mock<WebApiCoreSeedContext>();
-            var users = new Mock<DbSet<User>>();
+            // Arrange  
             var id = Guid.NewGuid();
-            User user = null;
-            context.Setup(x => x.Users)
-                   .Returns(users.Object);
-            users.Setup(x => x.FindAsync(It.Is<Guid>(y => y == id)))
-                 .ReturnsAsync(user)
+            _context.Setup(x => x.Users.FindAsync(It.Is<Guid>(y => y == id)))
+                 .ReturnsAsync((User)null)
                  .Verifiable();
-            var userService = new UserService(context.Object);
 
             //Act
-            var affectedRows = await userService.DeleteByIdAsync(Guid.NewGuid());
+            var affectedRows = await _userService.DeleteByIdAsync(Guid.NewGuid());
 
             // Assert
             Assert.Equal(0, affectedRows);
-            users.Verify(x => x.Remove(It.Is<User>(y => y.Id == id)), Times.Never);
-            context.VerifyAll();
+            _context.Verify(x => x.Users.Remove(It.Is<User>(y => y.Id == id)), Times.Never);
         }
 
         #endregion
@@ -125,51 +120,40 @@ namespace Seed.Domain.Tests
         [Fact]
         public async void Update_ShouldUpdateIfUserExists()
         {
-            // Arrange
-            var context = new Mock<WebApiCoreSeedContext>();
-            var users = new Mock<DbSet<User>>();
+            // Arrange    
             var user = GetADefaultUser();
-            context.Setup(x => x.Users)
-                   .Returns(users.Object);
-            users.Setup(x => x.FindAsync(It.Is<Guid>(y => y == user.Id)))
+            _context.Setup(x => x.Users.FindAsync(It.Is<Guid>(y => y == user.Id)))
                  .ReturnsAsync(user)
                  .Verifiable();
-            context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
+            _context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
                    .ReturnsAsync(1);
-            var userService = new UserService(context.Object);
 
             // Act
-            int affectedRows = await userService.UpdateAsync(user);
+            int affectedRows = await _userService.UpdateAsync(user);
 
             // Assert
             Assert.True(affectedRows > 0);
-            context.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
-            context.VerifyAll();
+            _context.Verify(x => x.Users.FindAsync(It.Is<Guid>(y => y == user.Id)), Times.Once);
+            _context.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
         }
 
         [Fact]
         public async void Update_ShouldReturnZeroIfUserNotExists()
         {
             // Arrange
-            var context = new Mock<WebApiCoreSeedContext>();
-            var users = new Mock<DbSet<User>>();
             var createdUser = GetADefaultUser();
-            context.Setup(x => x.Users)
-                   .Returns(users.Object);
-
-            var id = Guid.NewGuid();
-            users.Setup(a => a.FindAsync(It.Is<Guid>(g => g == id), It.IsAny<CancellationToken>()))
-                 .Returns<User>(null);
-            context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
-                   .ReturnsAsync(1);
-            var userService = new UserService(context.Object);
+            _context.Setup(a => a.Users.FindAsync(It.Is<Guid>(g => g == createdUser.Id)))
+                    .ReturnsAsync((User)null);
+            _context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(1);
 
             // Act
-            var affectedRows = await userService.UpdateAsync(createdUser);
+            var affectedRows = await _userService.UpdateAsync(createdUser);
 
             // Assert
             Assert.Equal(0, affectedRows);
-            context.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Never);
+            _context.Verify(x => x.Users.FindAsync(It.Is<Guid>(y => y == createdUser.Id)), Times.Once);
+            _context.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Never);
         }
 
         #endregion
@@ -179,23 +163,20 @@ namespace Seed.Domain.Tests
         [Fact]
         public async void Create_ShoulReturnOneIfCreated()
         {
-            // Arrange
-            var context = new Mock<WebApiCoreSeedContext>();
-            var users = new Mock<DbSet<User>>();
+            // Arrange    
             var createdUser = GetADefaultUser();
-            context.Setup(x => x.Users)
-                   .Returns(users.Object);
-            users.Setup(x => x.Add(It.Is<User>(y => y.Id == createdUser.Id)))
-                 .Verifiable();
-            context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
+            _context.Setup(x => x.Users.Add(It.Is<User>(y => y.Id == createdUser.Id)))
+                    .Verifiable();
+            _context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
                    .ReturnsAsync(1);
-            var userService = new UserService(context.Object);
+
             // Act
-            var affectedRows = await userService.CreateAsync(createdUser);
+            var affectedRows = await _userService.CreateAsync(createdUser);
+
             // Assert
             Assert.Equal(1, affectedRows);
-            context.VerifyAll();
-            users.Verify(x => x.Add(It.Is<User>(y => y.Id == createdUser.Id)), Times.Once);
+            _context.Verify(x => x.Users.Add(It.Is<User>(y => y.Id == createdUser.Id)), Times.Once);
+            _context.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
         }
 
         #endregion
